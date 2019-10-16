2c30a761d1aa366cc989f868b3a1639c11f11ca7
Massimo Bonanni <mabonann@microsoft.com>
2019-10-16 11:52:10 +0200

    Added extensions project Adde SQLTriggerAttribute

---
+++ SQLServerless.Extensions/Triggers/SQLTriggerAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.WebJobs.Description;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLServerless.Extensions.Triggers
+{
+    [Binding]
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public class SQLTriggerAttribute : Attribute
+    {
+        public SQLTriggerAttribute(string tableName, string keyName)
+        {
+            this.TableName = tableName;
+            this.KeyName = keyName;
+        }
+
+        [AppSetting(Default = "ConnectionString")]
+        [AutoResolve]
+        public string ConnectionString { get; set; }
+
+        public string TableName { get; internal set; }
+
+        public string KeyName { get; internal set; }
+    }
+}
