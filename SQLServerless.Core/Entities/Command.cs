6de070db606415ad392628f64b659d90d6faa122
Massimo Bonanni <mabonann@microsoft.com>
2019-10-16 09:02:10 +0200

    Added command for binding

---
+++ SQLServerless.Core/Entities/Command.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace SQLServerless.Core.Entities
+{
+    public class Command
+    {
+        public string CommandText { get; set; }
+
+        public Dictionary<string, object> Parameters { get; set; }
+
+        public bool IsCommandText { get; set; }
+
+        #region Constructors
+
+        public Command()
+        {
+            this.Parameters = new Dictionary<string, object>();
+            this.IsCommandText = false;
+        }
+
+        public Command(string commandText) : this()
+        {
+            this.CommandText = commandText;
+        }
+
+        public Command(string commandName, Dictionary<string, object> parameters) : this()
+        {
+            this.CommandText = commandName;
+            if (parameters != null)
+            {
+                this.Parameters = parameters;
+            }
+        }
+
+        #endregion
+       
+    }
+}
