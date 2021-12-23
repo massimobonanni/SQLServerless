e035e626e3f4de8bddbdd62834ccf69f5f05c249
Renan Pereira <rpereira@glasslewis.com>
2021-12-23 15:05:50 +0000

    update to aspnet standard and target samples to aspnet 6

---
+++ Test/SQLServerless.SQLCore.Console/Worker.cs
@@ -0,0 +1,34 @@
+﻿using SQLServerless.Core.Interfaces;
+
+namespace SQLServerless.SQLCore.Console
+{
+    public class Worker : BackgroundService
+    {
+        private readonly IChangeTracker _changeTracker;
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(IChangeTracker changeTracker, ILogger<Worker> logger)
+        {
+            _changeTracker = changeTracker;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            const string tableName = "dbo.Contacts";
+            const string key = "Id";
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("looking for changes in {tableName} based on primary key {primaryKey}", tableName, key);
+                var changes = await _changeTracker.GetChangesAsync(tableName, key, stoppingToken);
+                if (changes != null && changes.Rows.Any())
+                {
+                    foreach (var item in changes.Rows)
+                        _logger.LogInformation("processed {@item}", item);
+                }
+                await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+            }
+        }
+    }
+}
